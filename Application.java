6817589f9bd98fd32f19ca14f37e0ad55fd8e988
andreisafin <safin12345@yandex.ru>
2016-09-19 22:58:39 +0300

    Add files via upload

---
+++ Application.java
@@ -0,0 +1,7 @@
+package PO43.Safin.wdad;
+
+public  class Application {
+    public static void main(String[] ards) {
+        System.out.println("I am Andrey Safin, and I am not a monkey");
+    }
+}
