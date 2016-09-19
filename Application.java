12e2d62c4772d2d8389cefee2d50d323114d642e
Андрей <safin12345@yandex.ru>
2016-09-19 23:50:20 +0400

    Added entry point

--- Application.java
+++ Application.java
@@ -1,10 +1,7 @@
 package PO43.Safin.wdad;
 
-/**
- * Created by Андрей on 19.09.2016.
- */
 public  class Application {
-    public static void main(String[] ards){
+    public static void main(String[] ards) {
         System.out.println("I am Andrey Safin, and I am not a monkey");
     }
 }
