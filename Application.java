fb4ec7eb19c52dd600475e9894961ead6b4bcfeb
andreisafin <safin12345@yandex.ru>
2016-09-19 23:02:39 +0300

    Delete Application.java

--- Application.java
+++
@@ -1,7 +0,0 @@
-package PO43.Safin.wdad;
-
-public  class Application {
-    public static void main(String[] ards) {
-        System.out.println("I am Andrey Safin, and I am not a monkey");
-    }
-}
